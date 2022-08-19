eaedc1e357ab39fab7743c7a0113b95f857c3760
rawsonb <f.rawsonblackmon@gmail.com>
2022-08-19 18:21:48 -0400

    remove textFix function

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -25,22 +25,33 @@ fn start(cps: i32, rand: i32) {
         CLICKING = false;
     }
     thread::spawn(move || unsafe {
-        CLICKING = true;
+        if KEYBIND.len() != 0 {
+            CLICKING = true;
+        }
         println!("here");
         let mut enigo = Enigo::new();
         let mut rng = thread_rng();
         let device_state = DeviceState::new();
         let mut rng_val: i32 = rand;
         let mut rng_reset = 10;
+        let mut click = false;
         while CLICKING {
             let keys: Vec<Keycode> = device_state.get_keys();
-            if rng_reset > cps {
-                rng_val = rng.gen_range(rand - rand * 2..rand);
-                rng_reset = 0;
-            } else {
-                rng_reset += 1
+            if rng_val != 0 {
+                if rng_reset > cps {
+                    rng_val = rng.gen_range(rand - rand * 2..rand);
+                    rng_reset = 0;
+                } else {
+                    rng_reset += 1
+                }
             }
-            if keys == KEYBIND {
+            click = true;
+            for i in &KEYBIND {
+                if !keys.contains(i) {
+                    click = false;
+                }
+            }
+            if keys == KEYBIND && click {
                 enigo.mouse_click(MouseButton::Left);
                 thread::sleep(time::Duration::from_millis(
                     ((1000 / (cps + rng_val)) as i64).try_into().unwrap(),
@@ -52,11 +63,7 @@ fn start(cps: i32, rand: i32) {
 
 #[tauri::command]
 fn bind_key(key: String) -> String {
-    let keys_str: Vec<&str> = key.split(" ").collect();
-    let mut keys = vec![];
-    for s in &keys_str {
-        keys.push(fix_string(s.to_string()));
-    }
+    let keys: Vec<&str> = key.split(" ").collect();
     unsafe {
         KEYBIND = vec![];
     }
@@ -64,35 +71,9 @@ fn bind_key(key: String) -> String {
         unsafe {
             match device_query::Keycode::from_str(i) {
                 Ok(k) => KEYBIND.push(k),
-                _ => return ("Error").to_string(),
+                _ => return ("Bad Keybind").to_string(),
             }
         }
     }
     return key;
-}
-
-fn fix_string(s1: String) -> String {
-    let mut c = s1.chars();
-    if s1.len() == 1 {
-        match c.next() {
-            None => String::new(),
-            Some(f) => f.to_uppercase().to_string() + c.as_str(),
-        }
-    } else {
-        let mut output = "".to_string();
-        output = format!(
-            "{}{}{}",
-            output,
-            match c.next() {
-                None => String::new(),
-                Some(f) => f.to_uppercase().to_string(),
-            },
-            match c.next() {
-                None => String::new(),
-                Some(f) => f.to_uppercase().to_string() + c.as_str(),
-            }
-        );
-
-        return output;
-    }
 }
